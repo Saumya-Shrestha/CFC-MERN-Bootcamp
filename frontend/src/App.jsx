2c9398bc8648afa6ad7a45d2c999460a48d15cfe
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-03-20 22:23:30 +0545

    Initialized frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import TestClass from "./components/TestClass.jsx";
+
+function TestComponent() {
+  return (
+    <div>
+      <h1>Test Component</h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello everyone - code for change</h1>
+      <h2>Hello everyone</h2>
+      <TestComponent />
+      <TestClass />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/TestClass.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TestClass extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Class Test Component</h1>
+      </div>
+    );
+  }
+}
+
+export default TestClass;
